c69eb2e905e3ace28929c1608a4d33673d814ba1
Filip Dabrowski <ezarus1@gmail.com>
2024-05-20 22:14:50 +0200

    Removed unnecessary files

---
+++ src/summary.py
@@ -0,0 +1,32 @@
+import matplotlib as plt
+class Summary:
+    def __init__(self, sNum, sRange, tNum=None):
+        self.sensorNum = sNum
+        self.sensorRange = sRange
+        self.targetNum = tNum
+
+        #Log message containing number of currently active sensors and percentage of monitored area
+
+    def create_log_file(self, name):
+        print("logfile")
+
+    def simulation_log_message_area(self, sensors, fileName, subset):
+
+        with open("Logs/" + str(fileName), "a") as file:
+            activeSensorNum = 0
+            for sensor in sensors:
+                if sensor.isActive:
+                    activeSensorNum += 1
+            file.write("Subset:" + str(subset) + "\n" + "Active sensors: " + str(activeSensorNum) + "/" + str(self.sensorNum) + ". " + "Monitored area: " + str(activeSensorNum*(self.sensorRange/2)**2 * 3.14/10000) + "%\n")
+
+    def simulation_log_message_target(self, sensors, targets, fileName, subset):
+        with open("Logs/" + str(fileName), "a") as file:
+            activeSensorNum = 0
+            monitoredTargetsNum = 0
+            for sensor in sensors:
+                if sensor.isActive:
+                    activeSensorNum += 1
+            for target in targets:
+                if target.monitored:
+                    monitoredTargetsNum += 1
+            file.write("Subset:" + str(subset) + "\n" + "Active sensors: " + str(activeSensorNum) + "/" + str(self.sensorNum) + ". " + "Monitored targets: " + str(monitoredTargetsNum) + "/" + str(self.targetNum) + "\n")
