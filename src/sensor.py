24401e4f6ea6e64c6bfe556f60771c0fea2c98f6
Filip Dabrowski <ezarus1@gmail.com>
2024-05-25 17:18:04 +0200

    Minor changes in sensor.py

--- src/sensor.py
+++ src/sensor.py
@@ -2,15 +2,19 @@ from PyQt5.QtWidgets import QGraphicsEllipseItem
 from PyQt5.QtGui import QBrush, QColor
 import math 
 
+sensor_color_active = QColor(14, 236, 210)
+sensor_color_inActive = QColor(105, 105, 105)
+sensor_color_off = QColor(85, 85, 85)
+
 #Sensor class containg postition, range, and states for the sensor, representing it by the dot and semi-translucent range area
 class Sensor(QGraphicsEllipseItem):
-    def __init__(self, x, y, size, range):
+    def __init__(self, x: int, y: int, size: int, range: int):
         super().__init__(x, y, size, size) #initiaite parent object
         self.xPos = x
         self.yPos = y        
         self.range = range
         self.rangeAreaOpacity = 50 # Percentage value of the range area opacity
-        self.setBrush(QBrush(QColor(14, 236, 210)))  # Sensor color
+        self.setBrush(QBrush(sensor_color_active))  # Sensor color
 
         #Flags
         self.isActive = True
@@ -24,15 +28,15 @@ class Sensor(QGraphicsEllipseItem):
         return 
 
     def change_color_inactive(self):
-        self.setBrush(QBrush(QColor(105, 105, 105)))  # Sensor color
+        self.setBrush(QBrush(sensor_color_inActive))  # Sensor color
         self.range_area.setBrush(QBrush(QColor(3, 57, 51, self.rangeAreaOpacity))) # Range area color
 
     def change_color_active(self):
-        self.setBrush(QBrush(QColor(14, 236, 210)))  # Sensor color
+        self.setBrush(QBrush(sensor_color_active))  # Sensor color
         self.range_area.setBrush(QBrush(QColor(32, 179, 162, self.rangeAreaOpacity))) # Range area color
 
     def change_color_off(self):
-        self.setBrush(QBrush(QColor(85, 85, 85)))  # Sensor color
+        self.setBrush(QBrush(sensor_color_off))  # Sensor color
         self.range_area.setBrush(QBrush(QColor(3, 57, 51, 0))) # Range area color
 
     #Fade the sensor's area by the given percentage (Lower the opacity)
